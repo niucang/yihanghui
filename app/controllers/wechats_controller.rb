89c715a738f41fa08572f81dcb7ed3606bee55fb
chang.liu <chang.liu@alo7.com>
2018-09-08 22:58:59 +0800

    bug fix

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,5 +1,6 @@
 class WechatsController < ApplicationController
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#wechat_responder---rails-responder-controller-dsl
+  skip_before_action :authentication
   wechat_api
   wechat_responder
   def index

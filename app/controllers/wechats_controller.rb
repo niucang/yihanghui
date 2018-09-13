aebc5cbd4c58555a626041f979d56d91076f98b5
chang.liu <chang.liu@alo7.com>
2018-09-14 00:07:01 +0800

    bug fix

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -18,6 +18,6 @@ class WechatsController < ApplicationController
   private
     def reply_text request
       id = request[:EventKey].match(/[0-9]+/)[0]
-      request.reply.text "<a src=\"#{get_coupons_gift_share_url(id)}\">点我领取！</a>"
+      request.reply.text "谢谢关注！，<a src=\"#{get_coupons_gift_share_url(id)}\">点我领取！</a>"
     end
 end

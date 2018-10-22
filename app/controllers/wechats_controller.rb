f35d3c158d8a6ea7c782f9734e884b7d18aee1d7
chang.liu <chang.liu@alo7.com>
2018-10-22 21:28:57 +0800

    bug fix

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -22,7 +22,7 @@ class WechatsController < ApplicationController
 
   private
     def reply_text request
-      dump_json = JSON.parse request
+      dump_json = JSON.parse request[:EventKey]
       if dump_json.is_a? Hash
         url = get_coupons_gift_share_url(dump_json["spec"])
       else
